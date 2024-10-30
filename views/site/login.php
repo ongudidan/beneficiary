3e0467220ec53079f7d634e05b62ab274de654a1
ongudidan <ngudidan@gmail.com>
2024-10-30 23:33:32 +0300

    modified:   runtime/debug/index.data

--- views/site/login.php
+++ views/site/login.php
@@ -2,7 +2,6 @@
 
 /** @var yii\web\View $this */
 /** @var yii\bootstrap5\ActiveForm $form */
-
 /** @var app\models\LoginForm $model */
 
 use yii\bootstrap5\ActiveForm;
@@ -11,59 +10,54 @@ use yii\bootstrap5\Html;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
+<!-- Login 10 - Bootstrap Brain Component -->
+<section class=" vh-100 d-flex align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+                <div class="mb-5">
+                    <h4 class="text-center mb-4">Welcome back you've been missed!</h4>
+                </div>
+                <div class="card border border-light-subtle rounded-4">
+                    <div class="card-body p-3 p-md-4 p-xl-5">
+                        <?php $form = ActiveForm::begin([
+                            'id' => 'login-form',
+                            'options' => ['class' => 'p-3'],
+                        ]); ?>
 
-    <div class="main-wrapper login-body">
-        <div class="login-wrapper">
-            <div class="container">
-                <div class="loginbox">
-                    <div class="login-left">
-                        <img class="img-fluid" src="/web/dashboard/assets/img/login.png" alt="Logo">
-                    </div>
-                    <div class="login-right">
-                        <div class="login-right-wrap">
-                            <!-- <h1>Kaa Chonjo Dashboard</h1> -->
-                            <h2>Sign in</h2>
-
-                            <?php $form = ActiveForm::begin([
-                                'id' => 'login-form',
-                                'fieldConfig' => [
-                                    'template' => "{label}\n{input}\n{error}",
-                                    'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
-                                    'inputOptions' => ['class' => 'col-lg-3 form-control'],
-                                    'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
-                                ],
-                            ]); ?>
-
-                            <form action="index.html">
-                                <div class="form-group">
-                                    <label>Email <span class="login-danger">*</span></label>
-                                    <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
-                                    <span class="profile-views"><i class="fas fa-user-circle"></i></span>
+                        <p class="text-center mb-4" style="font-size: 1.25rem; font-weight: 500; color: #495057;">
+                            Sign in using <span class="text-primary">email</span>
+                        </p>
+                        <div class="row gy-3">
+                            <div class="col-12">
+                                <div class="form-floating mb-3">
+                                    <?= $form->field($model, 'email')->textInput(['autofocus' => true, 'class' => 'form-control', 'placeholder' => 'name@example.com'])->label('Email', ['class' => 'form-label']) ?>
                                 </div>
-                                <div class="form-group">
-                                    <label>Password <span class="login-danger">*</span></label>
-                                    <?= $form->field($model, 'password')->passwordInput() ?>
-                                    <span class="profile-views feather-eye toggle-password"></span>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-floating mb-3">
+                                    <?= $form->field($model, 'password')->passwordInput(['class' => 'form-control', 'placeholder' => 'Password'])->label('Password', ['class' => 'form-label']) ?>
                                 </div>
-                                <div class="forgotpass">
-                                    <div class="remember-me">
-                                        <?= $form->field($model, 'rememberMe')->checkbox([
-                                            'template' => "<div class=\"custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-                                        ]) ?> <span class="checkmark"></span>
-                                        </label>
-                                    </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-check">
+                                    <?= $form->field($model, 'rememberMe')->checkbox([
+                                        'template' => "<div class=\"form-check\">{input} {label}</div>\n",
+                                        'class' => 'form-check-input'
+                                    ])->label('Keep me logged in', ['class' => 'form-check-label text-secondary']) ?>
                                 </div>
-                                <div class="form-group">
-                                    <button class="btn btn-primary btn-block" type="submit">Login</button>
+                            </div>
+                            <div class="col-12">
+                                <div class="d-grid">
+                                    <button class="btn btn-primary btn-lg" type="submit">Log in</button>
                                 </div>
-                            </form>
-                            <?php ActiveForm::end(); ?>
-
+                            </div>
                         </div>
+
+                        <?php ActiveForm::end(); ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
